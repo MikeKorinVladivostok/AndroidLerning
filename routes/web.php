1cf86d902be2bd1d612d2b541532c06d1b982de4
mike <KorinetskiyMikhail@yandex.ru>
2021-12-14 12:01:44 +1000

    add controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CreateController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,12 +14,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/createpost', [CreateController::class, 'createTable']) ;
+
+Route::get('/firstcompany/', [\App\Http\Controllers\DeliveryController::class, 'firstDeliveryCompany']) ;
+
+Route::get('/secondcompany/', [\App\Http\Controllers\DeliveryController::class, 'secondDeliveryCompany']) ;
+
+
+//Route::get('/createpost', function () {
+//    return view('dashboard');
+//})->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
